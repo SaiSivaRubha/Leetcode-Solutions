6f4776d948a32725744d844efbd7a097428bccf6
SaiSivaRubha <saisivarubhakh@gmail.com>
2025-11-16 06:15:27 +0530

    added josephus.java

---
+++ backtracking+recursion/josephus.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findingValid(int n,int k){
+    if(n==1) return 0;
+        return (findingValid(n-1,k)+k)%n;
+    }
+    public int findTheWinner(int n, int k) {
+        int ans=findingValid(n,k)+1;
+        return ans;
+    }
+}
